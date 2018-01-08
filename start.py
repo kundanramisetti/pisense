30e967856f11f51138a30ad59c587f459cae7343
Kundan Ramisetti <kundan.r@me.com>
2018-01-08 18:27:48 +1300

    Added a init function to which will start and stop the alert lights

--- start.py
+++ start.py
@@ -4,11 +4,14 @@ import RPi.GPIO as GPIO, json, requests, logging
 # Custom modules
 import config
 
+# Overrides
+config.BOUNCE_TIME = 16000
+
 logging.basicConfig(filename=config.LOG_FILE_NAME, level=logging.DEBUG)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(config.PORT, GPIO.IN)
 
-def notify(*args) :
+def notify() :
     logging.info('Detected someone. Sending notification')
 
     slackResponse = requests.post(
@@ -16,7 +19,7 @@ def notify(*args) :
         headers={'Content-Type': 'application/json'}
     )
 
-    if slackResponse.status_code != 200:
+    if slackResponse.status_code != 200 :
         logging.error('Request to slack returned an error')
 
     hueResponse = requests.put(
@@ -24,10 +27,24 @@ def notify(*args) :
         data=json.dumps({"on": True, "alert": "lselect"})
     )
 
-    if hueResponse.status_code != 200:
+    if hueResponse.status_code != 200 :
         logging.error('Request to lights returned an error')
 
-GPIO.add_event_detect(config.PORT, GPIO.RISING, callback=notify, bouncetime=config.BOUNCE_TIME)
+def lightsOff() :
+    lightsResponse = requests.put(
+        config.LIGHTS_ENDPOINT + 'groups/' + str(config.LIGHTS_GROUP_ID) + '/action',
+        data=json.dumps({"on": False})
+    )
+
+    if lightsResponse.status_code != 200 :
+        logging.error('Request to lights returned an error')
+
+def init(*args) :
+    notify()
+    time.sleep(config.BOUNCE_TIME)
+    lightsOff()
+
+GPIO.add_event_detect(config.PORT, GPIO.RISING, callback=init, bouncetime=config.BOUNCE_TIME)
 
 while True :
     pass
